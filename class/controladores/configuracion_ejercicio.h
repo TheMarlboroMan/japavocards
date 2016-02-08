9ed60b102c81053c953510d638f6b704caee1d7c
TheMarlboroMan <marlborometal@gmail.com>
2016-02-08 22:25:43 +0100

    Cambios.

--- class/controladores/configuracion_ejercicio.h
+++ class/controladores/configuracion_ejercicio.h
@@ -7,11 +7,80 @@
 
 #include "../framework/controlador_interface.h"
 
+#include "../app/generador_listados.h"
 #include "../app/fuentes.h"
 #include "../app/configuracion_ejercicio.h"
 
 namespace App
 {
+
+/* Descripciones de los varios tipos de items de listado de configuración... 
+Necesitamos una clase base para poder meterlas en un vector de punteros. Por
+lo demás podemos usar las cosas del generador de listados. La interface de 
+listable realmente no vale para nada y sólo mete más ruido: el template
+del generador de listados es quien realmente hace el trabajo.
+*/
+
+namespace Listado_config
+{
+struct interface_item_config
+{
+	const DLibV::Fuente_TTF&	fuente;
+	Configuracion_ejercicion&	config;
+
+					interface_item_config(const DLibV::Fuente_TTF&, Configuracion_ejercicio&);
+	virtual void			al_salir()=0;
+	virtual void			al_pulsar(int)=0;
+	virtual void			al_down(int)=0;
+};
+
+struct item_config_direccion:
+	public interface_item_config,
+	public Listable
+{
+					item_config_direccion(const DLibV::Fuente_TTF&, Configuracion_ejercicio&);
+	virtual void			al_salir();
+	virtual void			al_pulsar(int);
+	virtual void			al_down(int);
+	virtual void 			generar_representacion_listado(DLibV::Representacion_agrupada& rep, int x, int y) const;
+};
+
+struct item_config_modo_etiquetas:
+	public interface_item_config,
+	public Listable
+{
+					item_config_modo_etiquetas(const DLibV::Fuente_TTF&, Configuracion_ejercicio&);
+	virtual void			al_salir();
+	virtual void			al_pulsar(int);
+	virtual void			al_down(int);
+	virtual void 			generar_representacion_listado(DLibV::Representacion_agrupada& rep, int x, int y) const;
+};
+
+struct item_config_numero_palabras:
+	public interface_item_config,
+	public Listable
+{
+					item_config_numero_palabras(const DLibV::Fuente_TTF&, Configuracion_ejercicio&);
+	virtual void			al_salir();
+	virtual void			al_pulsar(int);
+	virtual void			al_down(int);
+	virtual void 			generar_representacion_listado(DLibV::Representacion_agrupada& rep, int x, int y) const;
+};
+
+struct item_config_limite_palabras:
+	public interface_item_config,
+	public Listable
+{
+					item_config_limite_palabras(const DLibV::Fuente_TTF&, Configuracion_ejercicio&);
+	virtual void			al_salir();
+	virtual void			al_pulsar(int);
+	virtual void			al_down(int);
+	virtual void 			generar_representacion_listado(DLibV::Representacion_agrupada& rep, int x, int y) const;
+};
+
+
+}//Fin namespace listado config.
+
 class Controlador_configuracion_ejercicio:
 	public DFramework::Controlador_interface
 {
